2cb1c9a1eff4a40bce56f7b0d2f012915d1670d4
MrKnightmare007 <dassoumyadipta007@gmail.com>
2025-03-17 02:37:59 +0530

    Changed Day 1 Program 1

--- Day-01/D1P1.sol
+++ Day-01/D1P1.sol
@@ -1,7 +1,7 @@
 //)Print Hello Worlds
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.8.2 <0.9.0;
+pragma solidity ^0.8.0;
 
 contract HelloWorld {
     string public greet = "Hello World";
